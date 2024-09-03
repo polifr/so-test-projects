af8a69b9ee6656556eeb1d0dcd0b2629adb2f737
polifr <polifr@gmail.com>
2024-09-03 09:24:57 +0200

    Fix id

--- jpa-78940342/src/main/java/it/poli/entity/Group.java
+++ jpa-78940342/src/main/java/it/poli/entity/Group.java
@@ -1,6 +1,10 @@
 package it.poli.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
@@ -24,5 +28,8 @@ public class Group implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
   private Long id;
 }
